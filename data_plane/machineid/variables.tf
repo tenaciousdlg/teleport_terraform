79bd9f14f49154bb1e0e42c7b5284ae1c33dbdb6
chris de la garza <chris@chrisdlg.com>
2024-02-01 15:22:26 -0600

    v1.0 commit

--- data_plane/machineid/variables.tf
+++ data_plane/machineid/variables.tf
@@ -1,9 +1,28 @@
+##################################################################################
+# VARIABLES
+##################################################################################
 variable "proxy_service_address" {
   type        = string
-  description = "Host and HTTPS port of the Teleport Proxy Service"
+  description = "hostname of the teleport environment"
 }
 
 variable "aws_region" {
   type        = string
-  description = "Region in which to deploy AWS resources"
+  description = "region to deploy AWS resources"
 }
+
+variable "identity_path" {
+  type        = string
+  description = "file path location of identity file for teleport terraform provider"
+}
+
+variable "ec2_name" {
+  type        = string
+  description = "name of ec2 instance"
+}
+
+variable "teleport_major_version" {
+  type = string
+  description = "major version of teleport to use in userdata script"
+}
+##################################################################################
