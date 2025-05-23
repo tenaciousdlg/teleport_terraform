5d5b1fa34b4039c9d6cb21a60fd4cf519a369305
chris de la garza <chris@chrisdlg.com>
2025-05-23 10:16:36 -0600

    update with prod reference

---
+++ data_plane/mysql_self/environments/prod/variables.tf
@@ -0,0 +1,23 @@
+variable "env" {
+  description = "Environment name"
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-2"
+}
+
+variable "user" {
+  description = "Tag to identify the creator"
+  type        = string
+}
+
+variable "proxy_address" {
+  description = "Teleport Proxy address (without protocol)"
+  type        = string
+}
+
+variable "teleport_version" {
+  description = "Teleport version to install"
+  type        = string
+}
