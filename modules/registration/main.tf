b50214f64816a05f30d560419949f73fe0d98e40
chris de la garza <chris@chrisdlg.com>
2025-05-29 14:02:21 -0600

    module cleanup

--- modules/registration/main.tf
+++ modules/registration/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     teleport = {
       source  = "terraform.releases.teleport.dev/gravitational/teleport"
-      version = "~> 17.0"
     }
   }
 }
--- modules/registration/outputs.tf
+++ modules/registration/outputs.tf
@@ -1,3 +1,12 @@
 output "resource_name" {
   value = var.name
 }
+
+output "resource_type" {
+  value = var.resource_type
+}
+
+output "resource_id" {
+  value = var.resource_type == "database" ? teleport_database.this[0].id : teleport_app.this[0].id
+  description = "The ID of the registered Teleport resource"
+}
