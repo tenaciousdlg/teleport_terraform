fd245fd2ccacafc88870aa5c3cad6c7d71ba89c7
chris de la garza <chris@chrisdlg.com>
2025-05-29 18:00:13 -0600

    adding required provider

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
